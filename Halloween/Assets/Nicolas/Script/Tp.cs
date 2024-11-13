98b71cae244cd85a5d10df470f2fed7333d0ad24
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-11-13 19:25:46 -0300

    Menu Projetado
    Não irei conseguir fazer o terreno hoje. 😊

--- Halloween/Assets/Nicolas/Script/Tp.cs
+++ Halloween/Assets/Nicolas/Script/Tp.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Tp : MonoBehaviour
 {
+    public GameObject mainPart, credPainel;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            mainPart.SetActive(true);
+            credPainel.SetActive(false);
+        } 
+    }
+
     public void Teleport(string tp)
     {
         SceneManager.LoadScene(tp);
