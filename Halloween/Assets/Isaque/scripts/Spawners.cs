69958ca7a6e9a806cfccca9368c378660cce3793
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-11-21 09:05:42 -0300

    Round começo
    Ele spanam, mas estão esquisitos

---
+++ Halloween/Assets/Isaque/scripts/Spawners.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Spawners : MonoBehaviour
+{
+    public float spawningRate = 3f;
+    public GameObject[] enemyPrefab;
+    public Transform[] spawnPoints;
+
+    float lastSpawnTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lastSpawnTime + spawningRate < Time.time)
+        {
+            var randomSpawnPoints = spawnPoints[Random.Range(0, spawnPoints.Length - 1)];
+            var randomEnemy = enemyPrefab[Random.Range(0, spawnPoints.Length - 1)];
+            Instantiate(randomEnemy, randomSpawnPoints.position, Quaternion.identity);
+            lastSpawnTime = Time.time;
+            spawningRate *= 0.98f;
+        }
+    }
+}
+//https://www.youtube.com/watch?v=rWubVoGhFVs
